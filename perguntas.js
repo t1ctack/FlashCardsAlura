2c061f6040d7cb478234ad40fc26363375e97d7e
Luis Felipe <150393881+t1ctack@users.noreply.github.com>
2024-10-06 13:25:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Phyton?',
+    'O Phyton é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Oi em ingles?',
+    'Oi em ingles é Hi'
+)
